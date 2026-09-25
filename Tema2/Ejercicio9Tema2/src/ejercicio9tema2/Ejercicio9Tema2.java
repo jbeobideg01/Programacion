2d1be9b6d25c4150cc73d5818043b6cd02235874
jbeobideg01 <jbeobideg01@informatica.iesvalledeljerteplasencia.es>
2026-09-25 17:42:52 +0200

    Ejercicio9Tema2 subido

---
+++ Tema2/Ejercicio9Tema2/src/ejercicio9tema2/Ejercicio9Tema2.java
@@ -0,0 +1,20 @@
+package ejercicio9tema2;
+
+/**
+ *
+ * @author javie
+ */
+public class Ejercicio9Tema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static final double PI = 3.1415;
+    public static void main(String[] args) {
+        double radio = 3.55;//Declaro variable con su valor
+        double longitud;
+        longitud = 2 * PI * radio;//Calculo longitud con su formula
+        System.out.println("La longitud de una circuferencia cuyo radio vale 3 seria igual a: " + longitud + " metros");
+    }
+    
+}
